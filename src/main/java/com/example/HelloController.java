d51aa39e21dc46556139161fe1eeffa48d6304c8
Caroline Nordbrandt <caroline_nordbradt@hotmail.com>
2025-10-30 17:56:34 +0100

    Ändrat i controller och model för MVC - ListProperty för att meddelanden ska sparas chatt-liknade I controller - en observable som lyssnar på förändringar

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -11,32 +11,49 @@ import javafx.scene.layout.VBox;
  */
 public class HelloController {
 
+    //En model skapas som i bakgrunden är en lista och håller koll på meddelanden
     private final HelloModel model = new HelloModel();
-
+    //@FXML kopplingar
+    //Kopplar ett textfält från FXML där meddelandet skrivs
     @FXML
     private TextField messageInput;
 
+    //Kopplar en knapp från FXML som klickas på för att skickas
     @FXML
     private Button sendButton;
 
+    //Kopplar en låda från FXML där alla meddelanden visas
     @FXML
     private VBox chatBox;
 
+    //Metoden körs automatiskt när appen startar
     @FXML
     private void initialize() {
-
+        //Om användaren trycker på Enter eller klickar med musen -> skicka meddelandet
         messageInput.setOnAction((event) -> sendMessage());
         sendButton.setOnAction(event -> sendMessage());
+        //Listan i model som samlar meddelanden
+        model.messagesProperty()
+                //Lyssna på förändringar, observable = listan, oldList = listan innan förändringar, newList = listan efter förändringar
+                .addListener((observable, oldList, newList) -> {
+                    //Tar bort den gamla listan för att lägga till den nya
+                    chatBox.getChildren().clear();
+                    //lägg till varje meddelande som en pratbubbla
+                    for(String msg : newList) {
+                        //Skapar en etikett med stilen från css-klassen message-bubble
+                        Label label = new Label(msg);
+                        label.getStyleClass().add("message-bubble");
+                        //LÄgger till den i vbox
+                        chatBox.getChildren().add(label);
+                    }
+                });
 
     }
-//StringProperty - HelloModel
+
     private void sendMessage() {
-        String message = messageInput.getText();
-        if (message != null) {
-            Label messageLabel = new Label(message);
-            chatBox.getChildren().add(messageLabel);
+        //Lägger till meddelandet i listan från model och tömmer sedan fältet där text matas in(prompt-meddelande visas igen)
+            model.addMessage(messageInput.getText());
             messageInput.clear();
-            //messageInput.replaceSelection("Skriv ett nytt meddelande");
-        }
+
     }
-}
+    }
--- src/main/java/com/example/HelloModel.java
+++ src/main/java/com/example/HelloModel.java
@@ -1,5 +1,9 @@
 package com.example;
 
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.collections.FXCollections;
+
 /**
  * Model layer: encapsulates application data and business logic.
  */
@@ -7,11 +11,17 @@ public class HelloModel {
     /**
      * Returns a greeting based on the current Java and JavaFX versions.
      */
+        //Lista som håller alla meddelanden
+            //FXCollections.observableArrayList() = Nyckel som gör listan ändringsbar och som JavaFX kan lyssna på
+        private final ListProperty<String> messages = new SimpleListProperty<>(FXCollections.observableArrayList());
 
+        //getter från private
+        public ListProperty<String> messagesProperty() {
+            return messages;
+        }
+        //tar in ett meddelande och lägger till det i listan
+        public void addMessage(String message) {
+            messages.add(message);
+        }
 
-//    public String getGreeting() {
-//        String javaVersion = System.getProperty("java.version");
-//        String javafxVersion = System.getProperty("javafx.version");
-//        return "Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".";
-//    }
 }
