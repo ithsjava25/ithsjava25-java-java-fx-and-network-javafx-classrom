9061c4368c3e9698fc8336986b7f889cfa2ab30f
Linus Westling <Linus.westling@gmail.com>
2025-11-05 10:13:59 +0100

    Samma stadie som i lektionerna

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -1,14 +1,18 @@
 package com.example;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
 /**
  * Controller layer: mediates between the view (FXML) and the model.
  */
 public class HelloController {
 
-    private final HelloModel model = new HelloModel();
+    private final HelloModel model = new HelloModel(new NtfyConnectionImpl());
+    @FXML
+    public ListView<NtfyMessageDto> messageView;
 
     @FXML
     private Label messageLabel;
@@ -18,5 +22,10 @@ public class HelloController {
         if (messageLabel != null) {
             messageLabel.setText(model.getGreeting());
         }
+        messageView.setItems(model.getMessages());
+    }
+
+    public void sendMessage(ActionEvent actionEvent) {
+        model.sendMessage();
     }
 }
