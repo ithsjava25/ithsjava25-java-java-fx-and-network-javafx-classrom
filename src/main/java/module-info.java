ba9d641b2fcdc5230dd0b8c10a94f76cabbc4969
Rickard Ankar <rickard.ankar@iths.se>
2025-11-06 12:33:43 +0100

    working base

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module hellofx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires tools.jackson.databind;
+    requires io.github.cdimascio.dotenv.java;
+    requires java.net.http;
 
     opens com.example to javafx.fxml;
     exports com.example;
