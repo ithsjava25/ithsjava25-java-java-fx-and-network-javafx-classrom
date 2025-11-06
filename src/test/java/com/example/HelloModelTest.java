e182c0bbee8ae4b0437db5eab5b4ba948b1d2948
Adam Ottosson <addeecsgo@gmail.com>
2025-11-06 21:14:30 +0100

    setup wire mock and adds new test sendMessageToFakeServer

--- src/test/java/com/example/HelloModelTest.java
+++ src/test/java/com/example/HelloModelTest.java
@@ -1,10 +1,15 @@
 package com.example;
 
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
-
+@WireMockTest
 class HelloModelTest {
 
     @Test
@@ -21,6 +26,20 @@ class HelloModelTest {
 
         // Assert Then
         assertThat(spy.message).isEqualTo("Hello World");
+    }
+
+    @Test
+    void sendMessageToFakeServer(WireMockRuntimeInfo wmRuntimeInfo) {
+        var con = new NtfyConnectionImpl("http://localhost:" + wmRuntimeInfo.getHttpPort());
+        var model = new HelloModel(con);
+        model.setMessageToSend("Hello World");
+        stubFor(post("/mytopic").willReturn(ok()));
+
+        model.sendMessage();
+
+        // Verify call made to server
+        verify(postRequestedFor(urlEqualTo("/mytopic"))
+                .withRequestBody(matching("Hello World")));
     }
 
 }
