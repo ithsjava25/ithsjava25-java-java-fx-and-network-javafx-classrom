fbe851218e5f58688a76717c3cf1e5afeb7606d1
Adam Ottosson <addeecsgo@gmail.com>
2025-11-06 20:07:24 +0100

    Adds DI and tests

--- src/main/java/com/example/HelloModel.java
+++ src/main/java/com/example/HelloModel.java
@@ -1,15 +1,68 @@
 package com.example;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.github.cdimascio.dotenv.Dotenv;
+import javafx.application.Platform;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import tools.jackson.databind.ObjectMapper;
+import tools.jackson.databind.exc.IgnoredPropertyException;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.Objects;
+
 /**
  * Model layer: encapsulates application data and business logic.
  */
 public class HelloModel {
+
+     private final NtfyConnection connection;
+
+     private final ObservableList<NtfyMessageDto> messages = FXCollections.observableArrayList();
+     private final StringProperty messageToSend = new SimpleStringProperty();
+
+     public HelloModel(NtfyConnection connection) {
+         receiveMessage();
+         this.connection = connection;
+     }
+
+
+    public ObservableList<NtfyMessageDto> getMessages() {
+        return messages;
+    }
+
+    public String getMessageToSend() {
+        return messageToSend.get();
+    }
+
+    public StringProperty messageToSendProperty() {
+        return messageToSend;
+    }
+
+    public void setMessageToSend(String message) {
+         messageToSend.set(message);
+    }
+
     /**
      * Returns a greeting based on the current Java and JavaFX versions.
      */
     public String getGreeting() {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        return "Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".";
+        return "Chat Client by Adam";
+    }
+
+    public void sendMessage() {
+        connection.send(messageToSend.get());
+
+    }
+
+    public void receiveMessage() {
+        connection.receive(m->Platform.runLater(()->messages.add(m)));
     }
 }
