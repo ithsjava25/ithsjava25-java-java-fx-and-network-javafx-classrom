f4b6e96911c82bf36483617857b272b720648335
Mats Rönnqvist <mats.f.ronnqvist@gmail.com>
2025-11-09 20:09:59 +0100

    Update images and background images || practice stuff.

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -19,51 +19,26 @@ public class HelloController {
 
     private final HelloModel model = new HelloModel();
 
-        @FXML
-        private Label messageLabel;
-        @FXML
-        private Label currentDateAndTime;
-        @FXML
-        private Button updateButton;
+    @FXML
+    private Label messageLabel;
+    @FXML
+    private Label currentDateAndTime;
+    @FXML
+    private Button updateButton;
 
-        private Timeline timeline;
-        private boolean updateButtonState = true;
+    private Timeline timeline;
+    private boolean updateButtonState = true;
 
-        public HelloModel getModel() {
-            return model;
-        }
-        @FXML
-        ListView<String> listView;
-
-
-        @FXML
-        private void initialize() {
-
-            if (currentDateAndTime != null) {
-                currentDateAndTime.textProperty().bind(model.dateTimeProperty());
-            }
-
-            if (messageLabel != null) {
-                messageLabel.setText(model.getGreeting());
-            }
-
-            timeline = new Timeline(new KeyFrame(Duration.seconds(1), e -> {
-                model.setDateTime(LocalDateTime.now()
-                        .format(DateTimeFormatter.ofPattern("yyy-MM-dd hh:mm:ss")));
-            }));
-            timeline.setCycleCount(Timeline.INDEFINITE);
-            timeline.play();
-
-        }
-        public void updateButtonAction(ActionEvent actionEvent) {
-            if (updateButtonState) {
-                timeline.stop();
-            }
-            else {
-                timeline.play();
-            }
-
-            updateButton.setText(updateButtonState ? "Start" : "Stop");
-            updateButtonState = !updateButtonState;
-        }
+    public HelloModel getModel() {
+        return model;
     }
+
+    @FXML
+    ListView<String> listView;
+
+
+    @FXML
+    private void initialize() {
+
+    }
+}
--- src/main/java/com/example/HelloModel.java
+++ src/main/java/com/example/HelloModel.java
@@ -9,23 +9,6 @@ import javafx.scene.image.Image;
  */
 public class HelloModel {
 
-    private StringProperty dateTimeProperty;
-
-    public HelloModel() {
-        dateTimeProperty = new SimpleStringProperty();
-    }
-
-    public void setDateTime(String dateTime) {
-        dateTimeProperty.set(dateTime);
-    }
-
-    public String getDateTime() {
-        return dateTimeProperty.get();
-    }
-
-    public StringProperty dateTimeProperty() {
-        return dateTimeProperty;
-    }
     /**
      * Returns a greeting based on the current Java and JavaFX versions.
      */
@@ -34,14 +17,33 @@ public class HelloModel {
     Image smash2;
     Image smash3;
     Image smash4;
+    Image background;
 
-  /*  public HelloModel() {
-        noSmash = new Image();
-    }*/
+    public HelloModel() {
+        noSmash = new Image(getClass().getResource("/lantern.png").toExternalForm());
+        smash1 = new Image(getClass().getResource("/lantern1.png").toExternalForm());
+        smash2 = new Image(getClass().getResource("/lantern2.png").toExternalForm());
+        smash3 = new Image(getClass().getResource("/lantern3.png").toExternalForm());
+        smash4 = new Image(getClass().getResource("/lantern4.png").toExternalForm());
+        background = new Image(getClass().getResource("/background.png").toExternalForm());
+    }
 
-    public String getGreeting() {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        return "Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".";
+    public Image getNoSmash() {
+        return noSmash;
+    }
+    public Image getSmash1() {
+        return smash1;
+    }
+    public Image getSmash2() {
+        return smash2;
+    }
+    public Image getSmash3() {
+        return smash3;
+    }
+    public Image getSmash4() {
+        return smash4;
+    }
+    public Image getBackground() {
+        return background;
     }
 }
