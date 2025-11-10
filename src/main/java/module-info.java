6627f4551a6493294c8f2848cef0be87eb7202a9
Adam Majava <vonadamo@gmail.com>
2025-11-10 13:55:37 +0100

    Implement messaging functionality with UI updates and environment variable support

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,11 @@
 module hellofx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires io.github.cdimascio.dotenv.java;
+    requires java.net.http;
+    requires java.sql;
+    requires tools.jackson.databind;
+    requires java.desktop;
 
     opens com.example to javafx.fxml;
     exports com.example;
