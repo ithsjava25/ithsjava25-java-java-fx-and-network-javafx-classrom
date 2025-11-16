4679f9fb5f3297061d127d25ec00146d82fe1153
FMOUSSA <firasmoussa60@gmail.com>
2025-11-16 15:59:57 +0100

    Test raw client calls to ntfy.sh

--- src/main/java/com/example/HelloFX.java
+++ src/main/java/com/example/HelloFX.java
@@ -6,6 +6,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.UUID;
+
 public class HelloFX extends Application {
 
     @Override
@@ -18,8 +25,36 @@ public class HelloFX extends Application {
         stage.show();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        String baseUrl = "https://ntfy.sh";
+        String topic = "a5ce42c9-6d30-4f7b-942b-7e11d3075925";
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest sendRequest = HttpRequest.newBuilder()
+                .uri(URI.create(baseUrl + "/" + topic))
+                .POST(HttpRequest.BodyPublishers.ofString("I've been expecting you."))
+                .build();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(baseUrl + "/" + topic + "/json"))
+                .GET()
+                .build();
+
+        client.send(sendRequest, HttpResponse.BodyHandlers.discarding());
+
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofLines())
+                .thenAccept(response -> {
+                    response.body().forEach(line -> {
+                        System.out.println("Received raw line: " + line);
+                    });
+                });
+
+        System.out.println("Listening for messages on topic: " + topic);
+        Thread.sleep(Long.MAX_VALUE);
         launch();
+
     }
 
 }
